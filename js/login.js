3585d868a187853de51bc3189f3af1d8e231acd2
노주희 <sarah19270@naver.com>
2023-05-28 16:29:53 +0000

    자바웹프로그래밍 12주차

--- js/login.js
+++ js/login.js
@@ -34,52 +34,31 @@ function logout(){
 }
 
 function get_id(){
+	if(true){
+        decrypt_text();
+    }
+    else{
+		var getParameters = function(paramName){ // 변수 = 함수(이름)
+			var returnValue; // 리턴값을 위한 변수 선언
+			var url = location.href; // 현재 접속 중인 주소 정보 저장
+			var parameters = (url.slice(url.indexOf('?') + 1, url.length)).split('&'); // ?기준 slice 한 후 split 으로 나눔
+			for(var i = 0; i < parameters.length; i++) { 
+				var varName = parameters[i].split('=')[0];
+
+				if (varName.toUpperCase() == paramName.toUpperCase()) {
+					returnValue = parameters[i].split('=')[1];
+					return decodeURIComponent(returnValue);
+					// 나누어진 값의 비교를 통해 paramName 으로 요청된 데이터의 값만 return
+				}
+			} // 2중 for문 끝
+		}
+		alert(getParameters("id") + '님 반갑습니다!'); // 메시지 창 출력
+	}
 	
-    var getParameters = function(paramName){ // 변수 = 함수(이름)
-		var returnValue; // 리턴값을 위한 변수 선언
-		var url = location.href; // 현재 접속 중인 주소 정보 저장
-		var parameters = (url.slice(url.indexOf('?') + 1, url.length)).split('&'); // ?기준 slice 한 후 split 으로 나눔
-		for(var i = 0; i < parameters.length; i++) { 
-			var varName = parameters[i].split('=')[0];
 
-			if (varName.toUpperCase() == paramName.toUpperCase()) {
-				returnValue = parameters[i].split('=')[1];
-				return decodeURIComponent(returnValue);
-				// 나누어진 값의 비교를 통해 paramName 으로 요청된 데이터의 값만 return
-			}
-		} // 2중 for문 끝
-	}; // 함수 끝
+	
+};
 
-	alert(getParameters("id") + '님 반갑습니다!'); // 메시지 창 출력
-}
-
-function setCookie(name, value, expiredays){
-	var date = new Date();
-	date.setDate(date.getDate() + expiredays);
-	document.cookie = escape(name) + "=" + escape(value) + "; expires=" + date.toUTCString() + "SameSite=None; Secure";        
-}
-
-function getCookie(name) {
-        var cookie = document.cookie;
-        console.log("쿠키를 요청합니다.");
-        if (cookie != "") {
-            var cookie_array = cookie.split("; ");
-            for ( var index in cookie_array) {
-                var cookie_name = cookie_array[index].split("=");
-                
-                if (cookie_name[0] == "id") {
-                    return cookie_name[1];
-                }
-            }
-        }
-        return ;
-}
-
-function deleteCookie(cookieName){
-    var expireDate = new Date();
-    expireDate.setDate(expireDate.getDate() - 1);
-    document.cookie = cookieName + "= " + "; expires=" + expireDate.toGMTString();
-}
 
 function init(){ // 로그인 폼에 쿠키에서 가져온 아이디 입력
     let id = document.querySelector("#floatingInput");
@@ -92,31 +71,8 @@ function init(){ // 로그인 폼에 쿠키에서 가져온 아이디 입력
     }
 };
 
-function session_set(){
-	let id = document.querySelector("#floatingInput");
-	if(sessionStorage){
-		sessionStorage.setItem("Session_Storage_test", id.value);
-	}
-	else{
-		alert("로컬 스토리지 지원 x");
-	}
-}
 
-function session_get(){
-	if(sessionStorage){
-		return sessionStorage.getItem("Session_Storage_test");
-	}
-	else {
-		alert("세션 스토리지 지원 x");
-	}
-}
 
-function session_check(){
-	if(sessionStorage.getItem("Session_Storage_test")){
-		alert("이미 로그인 되었습니다");
-		location.href = 'index_login.html';
-	}
-}
 
 function init(){
 	let id = document.querySelector("floatingInput");
@@ -130,14 +86,15 @@ function init(){
     session_check(); // 세션 유무 검사
 }
 
-function session_del() {//세션 삭제
-    // Check if the sessionStorage object exists
-    if (sessionStorage) {
-        // Retrieve data
-        sessionStorage.removeItem("Session_Storage_test");
-        alert('로그아웃 버튼 클릭 확인 : 세션 스토리지를 삭제합니다.');
-    } 
-	else {
-        alert("세션 스토리지 지원 x");
-    }
+
+
+function addJavascript(jsname) { // 자바스크립트 외부 연동
+	var th = document.getElementsByTagName('head')[0];
+	var s = document.createElement('script');
+	s.setAttribute('type','text/javascript');
+	s.setAttribute('src',jsname);
+	th.appendChild(s);
 }
+addJavascript('/js/security.js'); // 암복호화 함수
+addJavascript('/js/session.js'); // 세션 함수
+addJavascript('/js/cookie.js'); // 쿠키 함수
